a7967f7d46ec5603833d147e7b90cf6a84177da1
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-09-23 10:14:33 +0100

    moved pages to view

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
-import About from '@/components/About'
-import Detail from '@/components/Detail'
-import Login from '@/components/Login'
-import Registration from '@/components/Registration'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Detail from '../views/Detail.vue'
+import Login from '../views/Login.vue'
+import Registration from '../views/Registration.vue'
 
 Vue.use(Router)
 
